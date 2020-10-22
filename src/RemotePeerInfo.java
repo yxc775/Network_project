5f238330a0e097e8f314324c0039b474803fa6e3
yxc775 <yxc775@case.edu>
2020-10-22 15:20:33 -0400

    Lay down ground work and design project structure

---
+++ src/RemotePeerInfo.java
@@ -0,0 +1,11 @@
+public class RemotePeerInfo {
+	public String peerId;
+	public String peerAddress;
+	public String peerPort;
+	
+	public RemotePeerInfo(String pId, String pAddress, String pPort) {
+		peerId = pId;
+		peerAddress = pAddress;
+		peerPort = pPort;
+	}
+}
--- src/StartRemotePeers.java
+++ src/StartRemotePeers.java
@@ -1,12 +1,3 @@
-package StartRemotePeers;/*
- *                     CEN5501C Project2
- * This is the program starting remote processes.
- * This program was only tested on CISE SunOS environment.
- * If you use another environment, for example, linux environment in CISE 
- * or other environments not in CISE, it is not guaranteed to work properly.
- * It is your responsibility to adapt this program to your running environment.
- */
-
 import java.io.*;
 import java.util.*;
 
