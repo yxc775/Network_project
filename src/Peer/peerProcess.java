3f083657d03c8b01772c1660fc241b893bcb48db
yxc775 <yxc775@case.edu>
2020-10-31 17:22:29 -0400

    Add initial step when starting a process

--- src/Peer/peerProcess.java
+++ src/Peer/peerProcess.java
@@ -1,5 +1,7 @@
 package Peer;
 
+import Config.CommonInfoConfig;
+import Logger.Logger;
 import Message.Message;
 
 import java.net.ServerSocket;
@@ -16,6 +18,9 @@ public class peerProcess {
     public static volatile Hashtable<String, RemotePeerInfo> unchockedPeer = new Hashtable<String, RemotePeerInfo>(); // this is the unprefered peer, which is unchocked
     public boolean isFinished = false; //if this peer finish download this variable will turned to true
 
+    public peerProcess(String peerID){
+        this.PeerID = Integer.parseInt(peerID);
+    }
 
     //this will update all peerinfo from peerinfo.cfg to Hashtable, and unchocked peer
     public static void readPeerInfo()
@@ -51,5 +56,23 @@ public class peerProcess {
     public void CreateEmptyFile()
     {
 
+    }
+
+    public static void main(String[] args){
+        peerProcess process = new peerProcess(args[0]);
+        try{
+            //start logging message communication between peers
+            Logger.start("log_peer_" + process.PeerID + ".log");
+
+            CommonInfoConfig.readCommonInfo("Common.cfg");
+            readPeerInfo();
+            //TODO:initialize prefered peers
+
+            //assuming this peer does not have the file
+            boolean hasFile = false;
+        }
+        catch (Exception e){
+
+        }
     }
 }
