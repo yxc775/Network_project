8a53b535570ce61aad5a87c0bc508def0406c4de
ghming180 <54820565+ghming180@users.noreply.github.com>
2020-11-30 00:01:11 -0500

    Update ProcessListener.java

--- src/Peer/ProcessListener.java
+++ src/Peer/ProcessListener.java
@@ -1,21 +1,58 @@
 package Peer;
 
+import java.io.IOException;
 import java.net.ServerSocket;
+import java.net.Socket;
 
 
 //this is designed for handling receive message, take role as receiver. receiving message and call peerProcess to send message
 public class ProcessListener implements  Runnable{
     private int ProcessID; // this will be the origional process id
-    private ServerSocket ListeningSocket;// this will be the socket we used
+    private ServerSocket listeningSocket;// this will be the socket we used
+    private Socket peerSocket;
+    private int peerID;
+    public Thread sendingThread;
 
-    public ProcessListener(ServerSocket socket, int peerID){
+    public ProcessListener(){
 
+    }
+
+    public ProcessListener(ServerSocket ListeningSocket, int peerID){
+        this.listeningSocket = ListeningSocket;
+        this.peerID = peerID;
     }
 
     //this will take as a thread and receiving message from other process
     public void run()
     {
+        while(true){
+            try {
+                peerSocket = listeningSocket.accept();
+                sendingThread = new Thread(new RemoteHandler( peerSocket.getLocalPort(), peerID, false,
+                        peerSocket.getLocalSocketAddress().toString()));
+                peerProcess.PrintLog(peerID + " : The connection is established");
+                ProcessesManager.sendingThread.add(sendingThread);
+                sendingThread.start();
 
+            }catch(IOException e){
+                peerProcess.PrintLog(this.peerID + " Exception in connection: " + e.toString());
+            }
+        }
+
+
+    }
+
+    public void closeSocket()
+    {
+        try
+        {
+            if(!peerSocket.isClosed())
+                peerSocket.close();
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
     }
 
     //this will initialize this class. such as id and socket
