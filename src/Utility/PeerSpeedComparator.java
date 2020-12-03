e0ae134a5096687c03a80499dcc02e3e1d39ed26
ghming180 <54820565+ghming180@users.noreply.github.com>
2020-12-02 19:19:48 -0500

    Update PeerSpeedComparator.java

--- src/Utility/PeerSpeedComparator.java
+++ src/Utility/PeerSpeedComparator.java
@@ -1,12 +1,25 @@
 package Utility;
 
 import Peer.RemotePeerInfo;
-
 import java.util.Comparator;
 
 public class PeerSpeedComparator implements Comparator<RemotePeerInfo> {
     //todo need to finish this one to allow Best candidate selection when choosing prefered neighbors
     public int compare(RemotePeerInfo rm1, RemotePeerInfo rm2){
-        return 0;
+        if(rm1 == null && rm2 == null){
+            return 0;
+        }
+        else if(rm1 == null){
+            return 1;
+        }
+        else if(rm2 == null){
+            return -1;
+        }
+        else{
+           if (Double.toString(rm1.downloadSpeed).equals(Double.toString(rm2.downloadSpeed))){
+               return 0;
+           }
+           return rm1.downloadSpeed > rm2.downloadSpeed ? -1 : 1 ;
+        }
     }
 }
